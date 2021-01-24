bedd277d60ef4f9ad8ff8ba49016eaabe0e61038
Kate McKenzie <kate.j.mckenzie@gmail.com>
2021-01-24 16:27:00 +1300

    Parse sessions listing

--- overtrick/almanac/management/commands/_scrape_sessions.py
+++ overtrick/almanac/management/commands/_scrape_sessions.py
@@ -1,8 +1,11 @@
-from urllib.request import urlopen
-
+import requests
+from bs4 import BeautifulSoup
 
 def scrape(club_id):
     print(f'Scraping session data for club {club_id}')
+    url = f'https://pairs.bridgenz.co.nz/?club={club_id}'
 
-    html = urlopen(f'https://pairs.bridgenz.co.nz/?club={club_id}')
-    print(html.read())
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    print(soup.h2.text)
