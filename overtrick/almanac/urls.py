adcb89cfe989237653a74b010423ad1e797d88a1
Kate McKenzie <kate.j.mckenzie@gmail.com>
2021-01-26 07:58:16 +1300

    Ported layout.html from Overtrick v1 Tweaked layout of sidebar

--- overtrick/almanac/urls.py
+++ overtrick/almanac/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'almanac'
 urlpatterns = [
-    path('sessions/', views.index, name='index'),
-    path('sessions/<int:session_id>/', views.detail, name='detail'),
+    path('sessions/', views.index, name='session_list'),
+    path('sessions/<int:session_id>/', views.detail, name='session_detail'),
 ]
