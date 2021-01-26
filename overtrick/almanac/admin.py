15b122b0680890941d7b6e5ea14462ee4d161eff
Kate McKenzie <kate.j.mckenzie@gmail.com>
2021-01-27 10:31:14 +1300

    Add Pair inline for Session admin

--- overtrick/almanac/admin.py
+++ overtrick/almanac/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 
-from .models import Session
+from almanac.models import Session
+from pairs.models import Pair
 
-admin.site.register(Session)
+
+class PairInline(admin.TabularInline):
+    model = Pair
+
+
+@admin.register(Session)
+class SessionAdmin(admin.ModelAdmin):
+    list_display = ['club', 'date', 'time', 'event']
+    inlines = [PairInline]
