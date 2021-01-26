086e0eab8a6bac3b0e8b978de888414687cfb4ad
Kate McKenzie <kate.j.mckenzie@gmail.com>
2021-01-27 08:46:03 +1300

    Add time choices to Session model

--- overtrick/almanac/models.py
+++ overtrick/almanac/models.py
@@ -2,13 +2,19 @@ from django.db import models
 
 
 class Session(models.Model):
+    TIME_CHOICES = (
+        ('M', 'Morning'),
+        ('A', 'Afternoon'),
+        ('E', 'Evening'),
+    )
+
     club = models.CharField(max_length=20)
     date = models.DateField()
-    time = models.CharField(max_length=10)
+    time = models.CharField(max_length=1, choices=TIME_CHOICES)
     event = models.CharField(max_length=40)
 
     class Meta:
-        ordering = ['-date']
+        ordering = ['-date', 'time']
 
     def __str__(self):
         return f'{self.club}: {self.date} {self.time}'
